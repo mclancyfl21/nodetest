b6c897e7c9eb96de646a706a9c31bb47a057044e
Matthew <matthew@Matthews-MacBook-Pro.local>
2020-02-22 23:13:08 -0500

    Updates

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-
+var fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => console.log(`server is running on ${PORT}`));
+app.use(express.static(path.join(__dirname,'public')))
 app.get('/', (req,res) => {
 res.sendFile(path.join(__dirname, 'public', 'index.html'));
+//res.sendFile(path.join(__dirname, 'other.js'));
 })
+
+
+
 app.get('/button', (req,res) => {
     res.sendFile(path.join(__dirname, 'public', 'button.html'));
-    })
+    });
+  
---
+++ public/js/other.js
@@ -0,0 +1,8 @@
+
+function function1(n) {
+        document.getElementById('div6').innerHTML='this is a test to assure that shit is working';
+    };
+    
+//module.exports = {
+//function1
+//};
